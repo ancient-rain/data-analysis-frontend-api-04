0fc6ba32b4e49b51c728883fceead4bd207a9f10
henstecj <henstecj@rose-hulman.edu>
2018-02-14 22:24:24 -0500

    updated routes as branch name states

--- ScheduleApp/controllers/student.js
+++ ScheduleApp/controllers/student.js
@@ -1,5 +1,203 @@
 const STUDENT = require('../models/student');
 
+exports.getStudentsTaken = function (req, res, next) {
+    const name = req.params.name.toUpperCase();
+    const regex = new RegExp('.*' + name + '.*');
+    let takenStudents;
+
+    STUDENT.db.db.command({
+        distinct: 'lookup',
+        key: 'username',
+        query: {
+            courses: regex
+        }
+    }, (err, usernames) => {
+        if (err) {
+            console.log(err);
+        } else {
+            takenStudents = usernames.values;
+            if (takenStudents == null) {
+                console.log('Error: Unable to find list of students who haven\'t taken course');
+            } else {
+                STUDENT.aggregate([{
+                    $match: {
+                        $and: [{
+                            term: req.params.term
+                        }, {
+                            username: { $in: takenStudents }
+                        }]
+                    }
+                }, {
+                    $lookup: {
+                        from: "lookup",
+                        localField: "username",
+                        foreignField: "advisees",
+                        as: "advisor"
+                    }
+                }, {
+                    $project: {
+                        term: 1,
+                        username: 1,
+                        name: 1,
+                        year: 1,
+                        graduationDate: 1,
+                        minors: 1,
+                        majors: 1,
+                        advisor: {
+                            $filter: {
+                                input: '$advisor',
+                                as: 'advisor',
+                                cond: {
+                                    $eq: ['$$advisor.term', req.params.term],
+                                }
+                            }
+                        },
+                    }
+                }], (err, students) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        try {
+                            const studentArray = [];
+                            for (let i = 0; i < students.length; i++) {
+                                const data = students[i];
+                                const advisor = getAdvisorStudentInfoTerm(data.advisor[0]);
+                                const majorStr = createMajorsString(data.majors);
+                                const minorStr = createMinorsString(data.minors);
+                                const newStudent = createStudentInfoTerm(data, advisor, [], [], majorStr, minorStr);
+                                studentArray.push(newStudent);
+                            }
+
+                            res.status(200);
+                            res.json(studentArray);
+                        } catch (error) {
+                            handleError('Could not students who have taken given course', res, 404, next);
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
+
+exports.getStudentsNotTaken = function (req, res, next) {
+    const name = req.params.name.toUpperCase();
+    const regex = new RegExp('.*' + name + '.*');
+    let major = name.substring(0, 2);
+    if (major === 'CS') {
+        major = ['CS', 'SE'];
+    } else {
+        major = [major];
+    }
+
+    let returnStudents = null;
+    let takenStudents = null;
+
+    STUDENT.db.db.command({
+        distinct: 'lookup',
+        key: 'username',
+        query: {
+            $and: [{
+                term: req.params.term
+            },
+            {
+                type: 'Student'
+            },
+            {
+                majors: { $in: major }
+            }
+            ]
+        }
+    }, (err, usernames) => {
+        if (err) {
+            console.log(err);
+        } else {
+            returnStudents = usernames.values;
+        }
+    });
+
+    STUDENT.db.db.command({
+        distinct: 'lookup',
+        key: 'username',
+        query: {
+            courses: {
+                $in: [regex]
+            }
+        }
+    }, (err, usernames) => {
+        if (err) {
+            console.log(err);
+        } else {
+            takenStudents = usernames.values;
+            if (returnStudents == null || takenStudents == null) {
+                console.log('Error: Unable to find list of students who haven\'t taken course');
+            } else {
+                returnStudents = returnStudents.filter(function (el) {
+                    return takenStudents.indexOf(el) < 0;
+                });
+                STUDENT.aggregate([{
+                    $match: {
+                        $and: [{
+                            term: req.params.term
+                        }, {
+                            username: { $in: returnStudents }
+                        }]
+                    }
+                }, {
+                    $lookup: {
+                        from: "lookup",
+                        localField: "username",
+                        foreignField: "advisees",
+                        as: "advisor"
+                    }
+                }, {
+                    $project: {
+                        term: 1,
+                        username: 1,
+                        name: 1,
+                        year: 1,
+                        graduationDate: 1,
+                        minors: 1,
+                        majors: 1,
+                        advisor: {
+                            $filter: {
+                                input: '$advisor',
+                                as: 'advisor',
+                                cond: {
+                                    $eq: ['$$advisor.term', req.params.term],
+                                }
+                            }
+                        },
+                    }
+                }], (err, students) => {
+                    if (err) {
+                        handleError('Bad request!', res, 400, next);
+                    } else {
+                        try {
+                            const studentArray = [];
+                            for (let i = 0; i < students.length; i++) {
+                                const data = students[i];
+                                const advisor = getAdvisorStudentInfoTerm(data.advisor[0]);
+                                const majorStr = createMajorsString(data.majors);
+                                const minorStr = createMinorsString(data.minors);
+                                const newStudent = createStudentInfoTerm(data, advisor, [], [], majorStr, minorStr);
+                                studentArray.push(newStudent);
+                            }
+
+                            res.status(200);
+                            res.json(studentArray);
+                        } catch (error) {
+                            handleError('Could not find students who have not taken given course', res, 404, next);
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    );
+}
+
 exports.getStudentInfoByTerm = function (req, res, next) {
     const username = req.params.username.toUpperCase();
     const term = req.params.term;
@@ -132,7 +330,7 @@ function getTermsStudentInfoTerm(terms) {
 function getTermName(key) {
     const year = key.substring(0, 4);
     const term = key.substring(4);
-    switch(term) {
+    switch (term) {
         case '10':
             return `Fall ${year}`;
         case '20':
